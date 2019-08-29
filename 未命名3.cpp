402ba920fd6c8873d849c887f3725d10663c72ed
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-08-29 23:49:27 +0800

    aha

--- 未命名3.cpp
+++ 未命名3.cpp
@@ -1,16 +1,24 @@
-#include<stdio.h>
-void printN(int N){
-		if (N>0){
-			printN(N-1);
-			if(N%2==0){
-				printf("%d\n",N);
-			}
-		}
-		return;
+#include <stdio.h>
+
+#define MAXN 10
+
+int Sum ( int List[], int N ){
+	int i;
+	int sum=0;
+	for(i=0;i<N;i++){
+		sum+=List[i];
 	}
-int main(){
-	int N;
-	scanf("%d",&N);
-	printN(N);
-	return 0;
+	return sum;
+}
+
+int main ()
+{
+    int List[MAXN], N, i;
+
+    scanf("%d", &N);
+    for ( i=0; i<N; i++ )
+        scanf("%d", &List[i]);
+    printf("%d\n", Sum(List, N));
+
+    return 0;
 }
