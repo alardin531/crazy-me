4fb4d03924ec9d642a655ae9bc2ab93fce3fd48a
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-09-01 23:36:02 +0800

    haha

--- 未命名3.cpp
+++ 未命名3.cpp
@@ -2,26 +2,14 @@
 
 #define MAXN 10
 
-int Sum ( int List[], int N ){
-	int i;
-	int sum=0;
-	for(i=0;i<N;i++){
-		sum+=List[i];
-	}
-	return sum;
-}
+
 
 int main ()
 {
-    int List[MAXN], N, i;
-    
-    scanf("%d", &N);
-    for ( i=0; i<N; i++ )
-        scanf("%d", &List[i]);
-    printf("%d\n", Sum(List, N));
-	if(N%2==0){
-		printf("wrong\n");
-	}
-
+    int a,b,c;
+    scanf("%d",&a);
+    b=a*10;
+    c=b*12+2*a;
+    printf("%d",c);
     return 0;
 }
