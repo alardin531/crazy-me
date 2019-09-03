b78e7652293fe367b357552f67de6ed3d1549321
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-09-04 00:12:30 +0800

    we must win!!

--- 未命名3.cpp
+++ 未命名3.cpp
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 #define MAXN 10
+typedef float ElementType;
 
-
+ElementType Average( ElementType S[], int N ){
+	double sum=0;
+	int i;
+	for(i=0;i<N;i++){
+		sum+=S[i];
+	} 
+	double ave=sum/N;
+	return ave;
+} 
 
 int main ()
 {
-    int a,b,c;
-    scanf("%d",&a);
-    b=a*10;
-    c=b*12+2*a;
-    printf("%d",c);
+    ElementType S[MAXN];
+    int N, i;
+
+    scanf("%d", &N);
+    for ( i=0; i<N; i++ )
+        scanf("%f", &S[i]);
+    printf("%.2f\n", Average(S, N));
+
     return 0;
 }
+
