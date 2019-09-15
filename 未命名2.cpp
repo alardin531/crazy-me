7ef06c5ee55b84636e5d7248aa7dadad6108ca16
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-09-15 18:40:09 +0800

    afa

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -1,44 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 struct student{
-    int a;
-    struct student * next;
-};
+	int data;
+	struct student * next;
+}; 
 struct student * create(int n){
-    struct student *p1,*p2,*head;
-    p1=p2=(struct student *)malloc(sizeof(struct student));
-    p1->next=NULL;
-    p2->next=NULL;
-    for(int i=1;i<=n;i++){
-        if(i==1){
-            scanf("%d",&p1->a);
-            head=p1;
-            p2=p1;
-        }
-        else{
-            p1=(struct student *)malloc(sizeof(struct student));
-            scanf("%d",&p1->a);
-            p2->next=p1;
-            p2=p1;
-        }
-    }
-    p2->next=NULL;
-    return (head);
-}
-int main (){
-    struct student *head,*p;
-    int n;
-    scanf("%d",&n);
-    head=p=create(n);
-    for(int i=1;i<=n;i++){
-        if(i!=n){
-        	printf("%d ",p->a);
+	struct student *head,*p1,*p2;
+	p1=p2=(struct student *)malloc(sizeof(struct student));
+	p1->next=NULL;
+	p2->next=NULL;
+	for(int i=1;i<=n;i++){
+		if(i==1){
+			scanf("%d",&p1->data);
+			head=p1;
+			p2=p1;
 		}
 		else{
-			printf("%d",p->a);
+			p1=(struct student *)malloc(sizeof(struct student));
+			scanf("%d",&p1->data);
+			p2->next=p1;
+			p2=p1;
 		}
-        p=p->next;
-    }
-    return 0;
+	}
+	p2->next=NULL;
+		return head;
+}
+int main(){
+	struct student*h,*m;
+	int n;
+	scanf("%d",&n);
+	h=m=create(n);
+	for(int i=0;i<n;i++){
+		if(i==n-1){
+			printf("%d",m->data);
+		}
+		else{
+			printf("%d ",m->data);
+		}
+		m=m->next;
+	}
+	return 0;
 }
 
