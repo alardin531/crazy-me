da75c9deaeacc51c1200af6fcb9d2c8a53b162f4
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-09-26 23:07:18 +0800

    sf

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -1,92 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
-//定义单链表 
-typedef int ElemType;
-typedef struct Node
-{
-	ElemType data;
-	struct Node *next;
-}Node;
-//1.栈的初始化 
-Node * InitStack()
-{
-	Node * p;
-	p=(Node *) malloc(sizeof(Node));
-	p->next=NULL;
-	return p;
-}
-//2.入栈操作 
-int Push(Node *top,ElemType x)
-{
-	 Node * p;
-	 p=(Node *) malloc(sizeof(Node));
-	 p->data=x;
-	 if(p==NULL)
-	   return 0;
-	 else
-	 {
-	 	p->next=top->next;
-	 	top->next=p;
-	 	
-	 	return 1;
-	 }
-} 
-//3.判空操作
-int Empty(Node *top)
-{
-	return top->next==NULL;
- } 
-//4.出栈操作 
-int Pop(Node *top,ElemType *ptr)
-{
-	Node * p;
-	if(Empty(top))
-	  return 0;
-	p=top->next;
-	top->next=p->next;
-	*ptr=p->data;
-	free(p);
-	return 1;
-} 
-//5.取栈顶元素 
-int GetTop(Node *top,ElemType *ptr)
-{
-    Node * p;
-	if(Empty(top))
-	  return 0;
-	p=top->next;
-	*ptr=p->data;
-	return 1;
-}
-int main()
-{
- 	ElemType x,*ptr;
- 	int i;
- 	
-	Node *top,*p;//定义链栈的栈顶指针并初始化 
-	top=InitStack();
-	Push(top,10);
-	Push(top,300);
-	Push(top,20);
-	
-	printf("*********\n");
-	p=top->next;
-	while(p)
-	{
-		printf("%d\n",p->data);
-		p=p->next;
+struct line{
+	int a;
+	struct line *next;
+}; 
+struct line *create(int *arr,int n){
+	struct line *NEW,*p1,*p2;
+	p1=p2=(struct line*)malloc(sizeof(struct line));
+	p1->next=NULL;
+	p2->next=NULL;
+	for(int i=0;i<n;i++){
+		if(i==0){
+			p1->a=arr[i];
+			NEW=p1;
+			p2=p1;
+		}
+		else{
+			p1=(struct line*)malloc(sizeof(struct line));
+			p1->a=arr[i];
+			p2->next=p1;
+			p2=p1;
+		}
 	}
-	
-	printf("*********\n");
-	if(Pop(top,&x))
-	  printf("%d\n",x);
-	
-	printf("*********\n");  
-	p=top->next;
-	while(p)
-	{
-		printf("%d\n",p->data);
-		p=p->next;
+	p2->next=NULL;
+	return NEW;
+}
+int main(){
+	struct line *p1,*p2;
+	int arr[100],n,k,s=0;
+	scanf("%d",&k);
+	while(k!=-1){
+		if(k%2!=0){
+			arr[s]=k;
+			s++;
+		}
+		scanf("%d",&k);
+	}
+	n=s;
+	p1=p2=create(arr,n);
+	for(int i=0;i<n;i++){
+		if(i==n-1){
+			printf("%d",p1->a);
+		}
+		else{
+			printf("%d ",p1->a);
+		}
+		p1=p1->next;
 	}
 	return 0;
 }
