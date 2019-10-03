bb67bd5a9d2a33bf7b7402a1cd42f87838757e50
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-10-03 14:19:05 +0800

    sc

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -5,48 +5,55 @@ struct Node {    //链表结点
     int data;    //数据
     struct Node* link;    //指向下一个结点的指针
 };
-/* 头插法建立单链表：返回单链表的头指针 */
-struct Node* buildLinkedList(int* arr, int n);    /* 头插法建立单链表 */
-void printLinkedList(struct Node* head);          /* 打印链表 */
+/* 尾插法建立单链表：返回单链表的头指针 */
+struct Node* buildLinkedList(int* arr, int n); /* 尾插法建立单链表 */
+void printLinkedList(struct Node* head);       /* 打印链表 */
 int main(int argc, char const *argv[]) {
     int n, i;
     int* a;
     scanf("%d", &n);
-    a = (int*)malloc(n * sizeof(int));    //动态内存分配申请数组空间
+    a = (int*)malloc(n * sizeof(int));	//动态内存分配申请数组空间
     for (i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
     }
-    struct Node* head = NULL;    //声明一个指针变量head
+    struct Node* head = NULL;	//声明一个指针变量head
     //创建链表，把返回的头指针赋值给head指针变量
     head = buildLinkedList(a, n);
     //打印链表：整个链表用head来代表。
     printLinkedList(head);
-    free(a);    //释放存储空间
+    free(a);	//释放存储空间
     return 0;
 }
 struct Node* buildLinkedList(int* arr, int n){
-	struct Node *head,*p1,*p2;
-	head=(struct Node*)malloc(sizeof(struct Node));
-	head->link=NULL;
+	struct Node* head,*p1,*p2;
+	p1=p2=(struct Node*)malloc(sizeof(struct Node));
+	p1->link=NULL;
+	p2->link=NULL;
 	for(int i=0;i<n;i++){
-		p1=(struct Node*)malloc(sizeof(struct Node));
-		p1->data=arr[i];
-		p1->link=head->link;
-		head->link=p1;
+		if(n==0){
+			p1->data=arr[i];
+			head=p1;
+			p2=p1;
+		}
+		else{
+			p1=(struct Node*)malloc(sizeof(struct Node));
+			p1->data=arr[i];
+			p2->link=p1;
+			p2=p1;
+		}
+		return head;
 	}
-	p2=head->link;
-	return (p2);
 }
 void printLinkedList(struct Node* head){
-	struct Node* p1,*p2;
-	int n=0;
+	int i=0;
+	struct Node *p1,*p2;
 	p1=p2=head;
 	while(p1!=NULL){
-		n++;
+		i++;
 		p1=p1->link;
 	}
-	for(int i=1;i<=n;i++){
-		if(i==n){
+	for(int j=0;j<i;j++){
+		if(j==i-1){
 			printf("%d",p2->data);
 		}
 		else{
@@ -54,4 +61,6 @@ void printLinkedList(struct Node* head){
 		}
 		p2=p2->link;
 	}
+	
 }
+
