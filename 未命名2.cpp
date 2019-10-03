4b2b2024eb7698a68b2e0c0d53e7386005a91a81
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-10-03 14:19:32 +0800

    ccc

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -1,66 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>//malloc函数
-
-struct Node {    //链表结点
-    int data;    //数据
-    struct Node* link;    //指向下一个结点的指针
-};
-/* 尾插法建立单链表：返回单链表的头指针 */
-struct Node* buildLinkedList(int* arr, int n); /* 尾插法建立单链表 */
-void printLinkedList(struct Node* head);       /* 打印链表 */
-int main(int argc, char const *argv[]) {
-    int n, i;
-    int* a;
-    scanf("%d", &n);
-    a = (int*)malloc(n * sizeof(int));	//动态内存分配申请数组空间
-    for (i = 0; i < n; ++i) {
-        scanf("%d", &a[i]);
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int date;
+struct node* next;
+}; 
+int main()
+{
+struct node* list=(struct node*)malloc(sizeof(struct node));
+    list->date=0;//x7fffffff;//创建头节点
+list->next=NULL;
+int k;
+scanf("%d",&k);
+int date;
+while(scanf("%d",&date)!=EOF&&date>=0)
+    { 
+struct node* newnode=(struct node*)malloc(sizeof(struct node));
+newnode->date=date;
+newnode->next=NULL;
+newnode->next=list->next;
+list->next=newnode;
     }
-    struct Node* head = NULL;	//声明一个指针变量head
-    //创建链表，把返回的头指针赋值给head指针变量
-    head = buildLinkedList(a, n);
-    //打印链表：整个链表用head来代表。
-    printLinkedList(head);
-    free(a);	//释放存储空间
-    return 0;
-}
-struct Node* buildLinkedList(int* arr, int n){
-	struct Node* head,*p1,*p2;
-	p1=p2=(struct Node*)malloc(sizeof(struct Node));
-	p1->link=NULL;
-	p2->link=NULL;
-	for(int i=0;i<n;i++){
-		if(n==0){
-			p1->data=arr[i];
-			head=p1;
-			p2=p1;
-		}
-		else{
-			p1=(struct Node*)malloc(sizeof(struct Node));
-			p1->data=arr[i];
-			p2->link=p1;
-			p2=p1;
-		}
-		return head;
-	}
-}
-void printLinkedList(struct Node* head){
-	int i=0;
-	struct Node *p1,*p2;
-	p1=p2=head;
-	while(p1!=NULL){
-		i++;
-		p1=p1->link;
-	}
-	for(int j=0;j<i;j++){
-		if(j==i-1){
-			printf("%d",p2->data);
-		}
-		else{
-			printf("%d ",p2->data);
-		}
-		p2=p2->link;
-	}
-	
+int count=1;
+struct node* p=list->next;
+while(p!=NULL)
+    {
+if(count==k)
+break;
+count++;
+        p=p->next;
+    } 
+if(p!=NULL)
+printf("%d",p->date);
+else
+printf("NULL");
 }
 
