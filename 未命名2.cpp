fa2b0cd45de47ba3fe84d6d7cfa66288c34af660
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-10-07 23:20:27 +0800

    dd

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -1,44 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct student{
-	int data;
-	struct student * next;
-}; 
-struct student * create(int n){
-	struct student *head,*p1,*p2;
-	p1=p2=(struct student *)malloc(sizeof(struct student));
-	p1->next=NULL;
-	p2->next=NULL;
-	for(int i=1;i<=n;i++){
-		if(i==1){
-			scanf("%d",&p1->data);
-			head=p1;
-			p2=p1;
-		}
-		else{
-			p1=(struct student *)malloc(sizeof(struct student));
-			scanf("%d",&p1->data);
-			p2->next=p1;
-			p2=p1;
-		}
-	}
-	p2->next=NULL;
-		return head;
-}
-int main(){
-	struct student*h,*m;
-	int n;
-	scanf("%d",&n);
-	h=m=create(n);
-	for(int i=0;i<n;i++){
-		if(i==n-1){
-			printf("%d",m->data);
-		}
-		else{
-			printf("%d ",m->data);
-		}
-		m=m->next;
-	}
-	return 0;
-}
+#include <stdio.h>
+#include <stdlib.h>//malloc函数
 
+struct Node {    //链表结点
+    int data;    //数据
+    struct Node* link;    //指向下一个结点的指针
+};
+/* 尾插法建立单链表：返回单链表的头指针 */
+struct Node* buildLinkedList(int* arr, int n);/* 尾插法建立单链表 */
+void printLinkedList(struct Node* head);      /* 打印链表 */
+int main(int argc, char const *argv[]) {
+    int n, i;
+    int* a;
+    scanf("%d", &n);
+    a = (int*)malloc(n * sizeof(int));	//动态内存分配申请数组空间
+    for (i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    struct Node* head = NULL;	//声明一个指针变量head
+    //创建链表，把返回的头指针赋值给head指针变量
+    head = buildLinkedList(a, n);
+    //打印链表：整个链表用head来代表。
+    printLinkedList(head);
+    free(a);	//释放存储空间
+    return 0;
+}
+struct Node* buildLinkedList(int* arr, int n){
+    struct Node *head,*p1,*p2;
+    p1=p2=(struct Node*)malloc(sizeof(struct Node));
+    p1->link=NULL;
+    p2->link=NULL;
+    for(int i=0;i<n;i++){
+    	if(i==0){
+    		p1->data=arr[i];
+    		head=p1;
+    		p2=p1;
+		}
+		else{
+			p1=(struct Node*)malloc(sizeof(struct Node));
+			p1->data=arr[i];
+			p2->link=p1;
+			p2=p1;
+		}
+	
+	}
+	p2->link=NULL;
+	return head;
+	}
+	void printLinkedList(struct Node* head){
+     struct Node *e,*m;
+     int n=0;
+	 e=head;
+	 m=head;
+	 while(e!=NULL){
+	 	n++;
+	 	e=e->link;
+	 } 
+	 for(int i=1;i<=n;i++){
+	 	if(i==n){
+	 		printf("%d",m->data);
+		 }
+		 else{
+		 	printf("%d ",m->data);
+		 }
+		 m=m->link;
+	 }
+	}
