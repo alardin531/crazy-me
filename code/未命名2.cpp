7c293e14463bc0010184b4be4c6fa432b1154ba6
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-10-27 23:43:32 +0800

    ggg

--- code/未命名2.cpp
+++ code/未命名2.cpp
@@ -1,73 +1,121 @@
-#include<stdio.h>
-#include<math.h>
-void guess(int a){
-	printf("\t\t\t\t欢迎使用2019款猜数字！\n\n\n");
-	printf("\t\t\t\t您的随机数生成中。。。。\n\n\n\n");
-	int key=a*2*8/3+11-33;
-	printf("\t\t\t\t您的随机数生成完毕！\n\n\n");
-	int g;
-	printf("\t\t\t\t请输入您的猜测：\n");
-	scanf("%d",&g);
-	while(g!=key){
-		if(g-key>0){
-			if(g-key>10000){
-				printf("数量级差的太远了！！\n");
-			}
-			else if(g-key>1000&&g-key<=10000){
-				printf("差距在1000以上！\n");
-			}
-			else if(g-key>100&&g-key<=1000){
-				printf("古有吕布辕门射戟，近有同学百步猜数\n");
-			}
-			else if(g-key>10&&g-key<=100){
-				printf("您的准星已经开始工作了！\n");
-			}
-			else if(g-key>1&&g-key<=10){
-				printf("您就快猜对了！不要放弃\n");
-			}
-		}
-		else{
-			int k=key-g;
-			if(k>10000){
-				printf("数量级差的太远了！！\n");
-			}
-			else if(k>1000&&k<=10000){
-				printf("差距在1000以上！\n");
-			}
-			else if(k>100&&k<=1000){
-				printf("古有吕布辕门射戟，近有同学百步猜数\n");
-			}
-			else if(k>50&&k<=100){
-				printf("您的准星已经开始工作了！\n");
-			}
-			else if(k>10&&k<=50){
-				printf("准星状态渐佳！\n");
-			}
-			else if(k>=1&&k<=10){
-				printf("您就快猜对了！不要放弃\n");
-			}
-		}
-		scanf("%d",&g);
-	}
-	printf("恭喜恭喜！总算猜对了!\n\n");
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#define ERROR 1e8
+typedef int ElementType;
+enum  Operation{push,pop,end};
+//typedef enum { false, true } bool;
+typedef int Position;
+struct SNode {
+    ElementType *Data;
+    Position Top1, Top2;
+    int MaxSize;
+};
+typedef struct SNode *Stack;
+Stack CreateStack(int MaxSize)
+{
+    Stack stack = (Stack)malloc(sizeof(struct SNode));
+    stack->Data = (int *)malloc(sizeof(ElementType)* MaxSize);
+    stack->Top1 = -1;
+    stack->Top2 = MaxSize;
+    stack->MaxSize = MaxSize;
+    return stack;
 }
-int main(){
-	
-	printf("\n\n\t\t\t\t\t下面将进入简易游戏世界！\n\n\t\t\t\t\tPowered by WangHe\n\t\t\t\t\t2019 all rights reserved.\n\n");
-	int a,b,c,d,n;
-	printf("\t\t\t\t\t请选择您需要的游戏：\n\n\n\t\t\t\t\t1.猜数字\n");
-	scanf("%d",&n);
-	switch (n){
-		case 1:{
-			printf("请随便输入一个幸运数字：");
-			scanf("%d",&a);
-			guess(a);
-			break;
-		}
-		default :{
-			printf("\n\n\t\t\t其他游戏正在开发中@-@\n");
-			break;
+bool Push(Stack S, ElementType X, int Tag)
+{
+    if (S == NULL)return false;
+    if (S->Top1+1==S->Top2){
+        printf("Stack Full\n");
+        return false;
+    }
+
+    if (Tag == 1)
+        S->Data[++S->Top1] = X;
+     else S->Data[--S->Top2] = X;
+     return true;
+}
+ElementType Pop(Stack S, int Tag)
+{
+    if (S == NULL)return ERROR;
+    if (Tag == 1){
+        if (S->Top1 == -1)
+        {
+            printf("Stack %d Empty\n",Tag);
+            return ERROR;
+        }
+        return S->Data[S->Top1--];
+    }
+    
+    if (S->Top2 == S->MaxSize)
+    {
+        printf("Stack %d Empty\n", Tag);
+        return ERROR;
+    }
+    return S->Data[S->Top2++];
+    
+    
+}
+
+Operation GetOp(){
+	char s[10];
+	scanf("%s",&s);
+    if(strcmp(s,"push")==0){
+    	return push;
+	}
+	if(strcmp(s,"pop")==0){
+		return pop;
+	}
+	if(strcmp(s,"end")==0){
+		return end;
+	}
+}
+void PrintStack(Stack S,int Tag){
+	if(Tag==1){
+		printf("pop from stack 1:\n");
+		for(int i=S->Top1;i>=0;i--){
+			printf("%d\n",S->Data[S->Top1]);
+			S->Top1--;
 		}
 	}
-	return 0;
+	if(Tag==2){
+		printf("pop from stack 2:\n");
+		for(int i=S->Top2;i<S->MaxSize;i++){
+			printf("%d\n",S->Data[S->Top2]);
+			S->Top2++;
+		}
+	}
 }
+
+int main()
+{
+    int N, Tag, X;
+    Stack S;
+    int done = 0;
+
+    scanf("%d", &N);
+    S = CreateStack(N);
+    while ( !done ) {
+        switch( GetOp() ) {
+        case push: 
+            {
+			scanf("%d %d", &Tag, &X);
+            if (!Push(S, X, Tag)) printf("Stack %d is Full!\n", Tag);}
+            break;
+        case pop:
+            {
+			scanf("%d", &Tag);
+            X = Pop(S, Tag);
+            if ( X==ERROR ) printf("Stack %d is Empty!\n", Tag);}
+            break;
+        case end:
+            {
+			PrintStack(S, 1);
+            PrintStack(S, 2);
+            done = 1;
+			}
+            break;
+        }
+    }
+    return 0;
+}
+
